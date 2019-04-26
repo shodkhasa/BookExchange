cbfa0ee82d4f4e9ecccd4e1d9fc975ba03ff70be
Sandip Hodkhasa <34456224+shodkhasa@users.noreply.github.com>
2019-04-25 20:35:17 -0700

    Update main.php
    Just added styling sheet and some text

--- main.php
+++ main.php
@@ -1,14 +1,17 @@
-<!doctype html>
-<html>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-<meta charset="utf-8">
+<link href="MainMenu.css" type="text/css" rel="stylesheet"/>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <title>Main Page</title>
-
 </head>
-
 <body>
-<?php
-	include("MainMenu.php");
-?>
+	<?php 
+		include("MainMenu.php");
+	?>
+    
+    <h2>Calstate La Book Exchange</h2>
+    <p>Welcome to the How To Do Library Research guide! Use the menu on the left to get research advice that will help you successfully navigate the Libraries' print and electronic collections. If you're new to the 	Libraries, you may want to visit our Services for Undergrads page or Services for Grad Students page to learn about the library resources and services that are available to you. If at any point you need help,	
+    you can Ask a Librarian or consult the Libraries' tutorials.</p>
 </body>
 </html>
